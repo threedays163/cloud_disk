b861294967824d193d8701b41192341c885b2733
zhangjing <zhangjing01@huatu.com>
2019-02-16 00:03:39 +0800

    启动项目时初始化hdfs,hbase连接

--- src/main/java/cs/whu/cloud/disk/DiskApplication.java
+++ src/main/java/cs/whu/cloud/disk/DiskApplication.java
@@ -1,15 +1,24 @@
 package cs.whu.cloud.disk;
 
+import cs.whu.cloud.disk.db.HbaseDB;
+import cs.whu.cloud.disk.db.HdfsDB;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
-public class DiskApplication {
+public class DiskApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DiskApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        //HbaseDB.getInstance();
+
+        //HdfsDB.getInstance();
+    }
 }
